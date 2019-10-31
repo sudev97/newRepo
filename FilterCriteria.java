dfa8073870243f1dbe0f325f5aa4bdaefd6f9ee9
sudev97 <sudevsnair97@gmail.com>
2019-10-31 12:03:45 +0530

    Added a basic hello world program(part of learning)

---
+++ FilterCriteria.java
@@ -0,0 +1,4 @@
+package pack1;
+public interface FilterCriteria {
+	public boolean filter(Employee e);
+}
---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+class HelloWorld{
+	public static void main(String[] args){
+		System.out.println( "Hello this is my first java program being pushed into git");
+	}
+}  
+
+
+
+
+
+
+
+
